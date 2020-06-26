f3388a0e69e937a2cdb81ed61202880dfbcedeeb
ms lee <leebar@naver.com>
2020-06-26 17:40:11 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,37 +20,54 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [
- {
-  'id': '1',
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '유관순',
-  'birthday': '961221',
-  'gender': '여자',
-  'job': '대학생'
-},
-{
-  'id': '2',
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생2'
-},
-{
-  'id': '3',
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '이순신',
-  'birthday': '961223',
-  'gender': '남자',
-  'job': '대학생3'
-}
-];
+// const customers = [
+//  {
+//   'id': '1',
+//   'image': 'https://placeimg.com/64/64/1',
+//   'name': '유관순',
+//   'birthday': '961221',
+//   'gender': '여자',
+//   'job': '대학생'
+// },
+// {
+//   'id': '2',
+//   'image': 'https://placeimg.com/64/64/2',
+//   'name': '홍길동',
+//   'birthday': '961222',
+//   'gender': '남자',
+//   'job': '대학생2'
+// },
+// {
+//   'id': '3',
+//   'image': 'https://placeimg.com/64/64/3',
+//   'name': '이순신',
+//   'birthday': '961223',
+//   'gender': '남자',
+//   'job': '대학생3'
+// }
+// ];
 
 // function App() {
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -68,9 +85,10 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              customers.map(c => {
+              // customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );
-              })
+              }) : null
             }
           </TableBody>
         </Table>
