0e248d87b835bbf936e432c91f25ae73dc21e9bc
ms lee <leebar@naver.com>
2020-06-29 13:28:36 +0900

    create customer database & node js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,33 +13,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     res.send({message: 'Hello Express!'});
 // });
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': '1',
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '유관순',
-            'birthday': '961221',
-            'gender': '여자',
-            'job': '대학생'
-          },
-          {
-            'id': '2',
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생2'
-          },
-          {
-            'id': '3',
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이순신',
-            'birthday': '961223',
-            'gender': '남자',
-            'job': '대학생3'
-          }
-    ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fileds) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
